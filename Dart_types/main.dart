e156d8f16f4166f4dfc8dbc7689e92b63b482843
henrymutega <hmutega@gmail.com>
2024-05-11 08:54:37 +0300

    Function commit.

--- Dart_types/main.dart
+++ Dart_types/main.dart
@@ -1,21 +1,33 @@
+String name = "Mercy"; //Persons name
+int age = 20; // string
+double wealth = 100.45; //Worthness of the person
+
+//Function to add two numbers
 void sum(int x, int y) {
   var total;
   total = x + y;
   print("The sum is: $total");
 }
 
-List<String> cars = ["BMW", "Nissan Note", "Benz", "Ford Ranger", "Porseche"];
+//A list of my all dream cars
+List<String> cars = ["BMW", "Nissan Note", "Benz", "Ford Ranger"];
+
+//A map of students names and their ages in a class
+Map<String, int> studentMarks = {
+  "Agnes": 14,
+  "Daniel": 16,
+  "James": 15,
+  "Jane": 13
+};
 
 void main() {
+  print("My name is $name" +
+      " I am $age" +
+      " years old." " My networth is $wealth" +
+      " million dollars.");
   sum(10, 20);
 
   cars.forEach((car) {
-    //print("This is my dream car: $car");
-    if (car == "Nissan Note" || car == "BMW") {
-      print("$car - Yes and Amen");
-    } else
-      () {
-        print("No object");
-      };
+    print("This is my dream car: $car");
   });
 }
